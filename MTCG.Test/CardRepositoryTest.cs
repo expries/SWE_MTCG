f7dede5a9accd31991b61cbaccf3fb0677dd7cf0
Samuel Fiedorowicz <if19b169@technikum-wien.at>
2020-11-17 15:58:11 +0100

    Card Controller and Repository Tests

--- MTCG.Test/CardRepositoryTest.cs
+++ MTCG.Test/CardRepositoryTest.cs
@@ -1,7 +1,6 @@
 using System;
-using System.Collections.Generic;
-using MTCG.Contracts.Requests;
 using MTCG.Repositories;
+using MTCG.Resources.Cards.SpellCards;
 using NUnit.Framework;
 
 namespace MTCG.Test
@@ -21,62 +20,42 @@ namespace MTCG.Test
         public void Test_CreateCard_ReturnsNewCardOnCreation()
         {
             // arrange
-            var request = new CardCreationRequest
-            {
-                Name = "WaterSpell",
-                Damage = 10.0,
-                Weakness = 10.0,
-                Id = Guid.NewGuid()
-            };
-            
+            var requestedCard = new WaterSpell("WaterSpell", 10, 10) {Id = Guid.NewGuid()};
+
             // act
-            var card = _repository.CreateCard(request.Id, request.Name, request.Damage, request.Weakness);
+            var card = _repository.CreateCard(requestedCard);
             
             // assert
-            Assert.AreEqual(request.Id, card.Id);
-            Assert.AreEqual(request.Name, card.Name);
-            Assert.AreEqual(request.Damage, card.Damage);
+            Assert.AreEqual(requestedCard.Id, card.Id);
+            Assert.AreEqual(requestedCard.Name, card.Name);
+            Assert.AreEqual(requestedCard.Damage, card.Damage);
         }
 
         [Test]
         public void Test_CreateCard_ReturnsNullIfCardWithIdAlreadyExists()
         {
             // arrange
-            var request = new CardCreationRequest
-            {
-                Name = "Goblin",
-                Damage = 10.0,
-                Weakness = 10.0,
-                Id = Guid.NewGuid()
-            };
-            
+            var requestedCard = new WaterSpell("WaterSpell", 10, 10) {Id = Guid.NewGuid()};
+
             // act
-            _repository.CreateCard(request.Id, request.Name, request.Damage, request.Weakness);
-            var card = _repository.CreateCard(request.Id, request.Name, request.Damage, request.Weakness);
+            _repository.CreateCard(requestedCard);
             
             // assert
-            Assert.IsNull(card);
+            Assert.Throws<ArgumentException>(() => _repository.CreateCard(requestedCard));
         }
 
         [Test]
         public void Test_GetCard_ReturnsCardIfItWasFound()
         {
             // arrange
-            var request = new CardCreationRequest
-            {
-                Name = "Goblin",
-                Damage = 10.0,
-                Id = Guid.NewGuid()
-            };
+            var requestedCard = new WaterSpell("WaterSpell", 10, 10) {Id = Guid.NewGuid()};
 
             // act
-            _repository.CreateCard(request.Id, request.Name, request.Damage);
-            var card = _repository.GetCard(request.Id);
+            _repository.CreateCard(requestedCard);
+            var card = _repository.GetCard(requestedCard.Id);
             
             // assert
-            Assert.AreEqual(request.Id, card.Id);
-            Assert.AreEqual(request.Name, card.Name);
-            Assert.AreEqual(request.Damage, card.Damage);
+            Assert.AreEqual(requestedCard, card);
         }
 
         [Test]
@@ -96,17 +75,11 @@ namespace MTCG.Test
         public void Test_DeleteCard_ReturnsTrueIfCardWasDeleted()
         {
             // arrange
-            var request = new CardCreationRequest
-            {
-                Name = "WaterSpell",
-                Damage = 10.0,
-                Weakness = 10.0,
-                Id = Guid.NewGuid()
-            };
+            var requestedCard = new WaterSpell("WaterSpell", 10, 10) {Id = Guid.NewGuid()};
 
             // act
-            _repository.CreateCard(request.Id, request.Name, request.Damage, request.Weakness);
-            bool deleted = _repository.DeleteCard(request.Id);
+            _repository.CreateCard(requestedCard);
+            bool deleted = _repository.DeleteCard(requestedCard.Id);
             
             // assert
             Assert.IsTrue(deleted);
