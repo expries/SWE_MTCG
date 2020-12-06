0bb61a681547614983b472c7d17a4c8462d984d4
Samuel Fiedorowicz <if19b169@technikum-wien.at>
2020-12-06 22:28:45 +0100

    Change ServiceResult to one generic Action Result

---
+++ MTCG/ActionResults/ServiceError.cs
@@ -0,0 +1,16 @@
+namespace MTCG.ActionResults
+{
+    public enum ServiceError
+    {
+        BadFormat,
+        DuplicateId,
+        PackageIsEmpty,
+        UserNotFound,
+        CardNotFound,
+        CardNotOwned,
+        NotEnoughCoins,
+        AllPackagesAcquired,
+        UsernameIsTaken,
+        DeckNotSet
+    }
+}
