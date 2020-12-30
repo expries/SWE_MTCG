f929bed6a653a53722cb9966f819064098387220
Samuel Fiedorowicz <if19b169@technikum-wien.at>
2020-12-30 21:19:57 +0100

    Controller and Service layer for accessing stats

---
+++ MTCG/Controller/StatsController.cs
@@ -0,0 +1,52 @@
+﻿using MTCG.Repositories;
+using MTCG.Server;
+using MTCG.Services;
+
+namespace MTCG.Controller
+{
+    public class StatsController : ApiController
+    {
+        private readonly IScoreService _scoreService;
+
+        public StatsController(IScoreService scoreService)
+        {
+            _scoreService = scoreService;
+        }
+
+        public ResponseContext GetStats(string token)
+        {
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                return BadRequest(new {Error = "Authorization is required."});
+            }
+
+            var result = _scoreService.GetStats(token);
+
+            if (!result.Success)
+            {
+                return NotFound(result.Error);
+            }
+
+            var stats = result.Value;
+            return Ok(stats);
+        }
+
+        public ResponseContext GetScoreboard(string token)
+        {
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                return BadRequest(new {Error = "Authorization is required."});
+            }
+            
+            var result = _scoreService.GetScoreboard(token);
+
+            if (!result.Success)
+            {
+                return Conflict(result.Error);
+            }
+
+            var stats = result.Value;
+            return Ok(stats);
+        }
+    }
+}
---
+++ MTCG/Services/IScoreService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using MTCG.Entities;
+using MTCG.Results;
+
+namespace MTCG.Services
+{
+    public interface IScoreService
+    {
+        public Result<StatsEntity> GetStats(string token);
+        
+        public Result<List<StatsEntity>> GetScoreboard(string token);
+    }
+}
